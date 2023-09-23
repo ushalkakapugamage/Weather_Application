7dbdaf47be98387a5d7d3516f9abea954733a85b
ushalka kapugamage <ushalkakapugamage@gmail.com>
2023-09-23 16:22:38 +0530

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+console.log("hi");
+
+const menu = document.getElementById("menu");
+menu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+const icon = document.getElementById("icon");
+const text = document.getElementById("theme-text");
+icon.onclick = () => {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")) {
+        icon.src="ASSETS/sun.png";
+        text.innerText="Light";
+        
+    }else {
+        icon.src="ASSETS/moon.png";
+        text.innerText="Dark";
+    }
+};
