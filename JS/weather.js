b64e7b71fe776dde7f8a2a76edf0c74aec5f69f6
ushalka kapugamage <ushalkakapugamage@gmail.com>
2023-09-27 23:53:17 +0530

    Connected api to display live weather details

--- JS/weather.js
+++ JS/weather.js
@@ -1,25 +1,62 @@
 const apiKey = "e0f51fbd4d5f4d9383433550232509";
 const apiUrl = "http://api.weatherapi.com/v1";
 
-const search_btn = document.getElementById("search-btn");
-search_btn.onclick = () => {
-    console.log("Search");
-    display_weather();
+let latitude = undefined;
+let longitude = undefined;
+
+
+window.onload = function () {
+    var x = document.getElementById("demo");
+    var latitude;  // Variable to store latitude
+    var longitude; // Variable to store longitude
+
+    var getLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    };
+
+    var showPosition = function (position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+
+        console.log("Latitude: " + latitude);
+        console.log("Longitude: " + longitude);
+        maincard_weather(latitude, longitude);
+        forcast_weather(latitude, longitude);
+    };
+
+    // Call the getLocation function to initiate the geolocation process
+    getLocation();
 
 
 };
 
-function display_weather() {
-    maincard_weather();
-    forcast_weather();
+
+
+
+const search_btn = document.getElementById("search-btn");
+search_btn.onclick = () => {
+    let search_bar = document.getElementById("search-bar").value;
+    const location = encodeURIComponent(search_bar);
+    display_weather(location);
+
+
+};
+
+function display_weather(location) {
+    maincard_weather(location);
+    forcast_weather(location);
 }
 
-function maincard_weather() {
-    let search_bar = document.getElementById("search-bar").value;
+function maincard_weather(location) {
+
 
     // Use encodeURIComponent to properly encode the search_bar value
-    const encodedSearchBar = encodeURIComponent(search_bar);
-    fetch(`${apiUrl}/current.json?key=${apiKey}&q=${encodedSearchBar}`)
+
+    fetch(`${apiUrl}/current.json?key=${apiKey}&q=${location}`)
         .then(response => {
             // Check if the response status is OK (200)
             if (!response.ok) {
@@ -36,11 +73,56 @@ function maincard_weather() {
             document.getElementById("cell3").innerHTML = data.location.name;
             let conditionArray = condition.split(' ');
 
-            let imgElement = document.querySelector("#main-img");
+            // Change the image source based on the condition
+            if (conditionArray[conditionArray.length - 1] == "shower") {
+                document.getElementById("main-img").src = "ASSETS/rainy.webp"; // Change to the new image source
+            }
+
+            const windPressure = data.current.pressure_mb;
+            const windSpeed = data.current.wind_kph;
+            const humidity = data.current.humidity;
+            const uvIndex = data.current.uv;
+            const sunrise = data.location.localtime;
+            const sunset = data.location.localtime;
+
+            // Update the table cells with the data
+            document.getElementById('windPressure').textContent = windPressure;
+            document.getElementById('windSpeed').textContent = windSpeed;
+            document.getElementById('humidity').textContent = humidity;
+            document.getElementById('uvIndex').textContent = uvIndex;
+            document.getElementById('sunrise').textContent = sunrise;
+            document.getElementById('sunset').textContent = sunset;
+        })
+        .catch(error => {
+            // Handle errors here
+            console.error('Fetch error:', error);
+        });
+}
+function maincard_weather(latitude, longitude) {
+
+
+    // Use encodeURIComponent to properly encode the search_bar value
+
+    fetch(`${apiUrl}/current.json?key=${apiKey}&q=${latitude},${longitude}`)
+        .then(response => {
+            // Check if the response status is OK (200)
+            if (!response.ok) {
+                throw new Error("Weather data request failed with status: " + response.status);
+            }
+            // Parse the response as JSON
+            return response.json();
+        })
+        .then(data => {
+            // Handle the JSON data here
+            console.log(data);
+            document.getElementById("cell1").innerHTML = data.current.temp_c + "&deg;";
+            let condition = document.getElementById("cell2").innerHTML = data.current.condition.text;
+            document.getElementById("cell3").innerHTML = data.location.name;
+            let conditionArray = condition.split(' ');
 
             // Change the image source based on the condition
-            if (conditionArray[conditionArray.length - 1] == "rain") {
-                imgElement.img.src = "ASSETS/rainy.webp"; // Change to the new image source
+            if (conditionArray[conditionArray.length - 1] == "shower") {
+                document.getElementById("main-img").src = "ASSETS/rainy.webp"; // Change to the new image source
             }
 
             const windPressure = data.current.pressure_mb;
@@ -64,12 +146,8 @@ function maincard_weather() {
         });
 }
 
-function forcast_weather() {
-    let search_bar = document.getElementById("search-bar").value;
-
-    // Use encodeURIComponent to properly encode the search_bar value
-    const encodedSearchBar = encodeURIComponent(search_bar);
-    fetch(`${apiUrl}/forecast.json?key=${apiKey}&q=${encodedSearchBar}&days=5`)
+function forcast_weather(location) {
+    fetch(`${apiUrl}/forecast.json?key=${apiKey}&q=${location}&days=5`)
         .then(response => {
             // Check if the response status is OK (200)
             if (!response.ok) {
@@ -83,20 +161,64 @@ function forcast_weather() {
             console.log(data);
 
 
-            for (let i = 0; i < 5; i++) {
+            for (let i = 1; i < 5; i++) {
                 // Use template literals to concatenate the index i with the IDs
                 document.getElementById(`card-temp${i}`).innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "&deg;";
                 let condition = document.getElementById(`card-condition${i}`).innerHTML = data.forecast.forecastday[i].day.condition.text;
+                document.getElementById(`card-date${i}`).innerHTML = data.forecast.forecastday[i].date;
+            
                 let conditionArray = condition.split(' ');
                 if (conditionArray[conditionArray.length - 1] == "rain") {
                     document.getElementById(`card${i}`).style.backgroundImage = "url('Assets/rainy.webp')";
+                } else if (conditionArray[conditionArray.length - 1] == "cloudy") {
+                    document.getElementById(`card${i}`).style.backgroundImage = "url('Assets/clouds.jpeg')";
+                } else if (conditionArray[conditionArray.length - 1] == "sunny") {
+                    document.getElementById(`card${i}`).style.backgroundImage = "url('Assets/sun.jpg')";
                 }
+                // Add more conditions as needed for other weather conditions
             }
+            
 
         })
         .catch(error => {
             // Handle errors here
             console.error('Fetch error:', error);
         });
+
+    function forcast_weather(latitude, longitude) {
+        fetch(`${apiUrl}/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=5`)
+            .then(response => {
+                // Check if the response status is OK (200)
+                if (!response.ok) {
+                    throw new Error("Weather data request failed with status: " + response.status);
+                }
+                // Parse the response as JSON
+                return response.json();
+            })
+            .then(data => {
+                // Handle the JSON data here
+                console.log(data);
+
+
+                for (let i = 1; i < 5; i++) {
+                    // Use template literals to concatenate the index i with the IDs
+                    document.getElementById(`card-temp${i}`).innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "&deg;";
+                    let condition = document.getElementById(`card-condition${i}`).innerHTML = data.forecast.forecastday[i].day.condition.text;
+                    document.getElementById(`card-date${i}`).innerHTML = data.forecast.forecastday[i].date;
+
+                    let conditionArray = condition.split(' ');
+                    if (conditionArray[conditionArray.length - 1] == "rain") {
+                        document.getElementById(`card${i}`).style.backgroundImage = "url('Assets/rainy.webp')";
+                    } else if (conditionArray[conditionArray.length - 1] == "cloudy") {
+                        document.getElementById(`card${i}`).style.backgroundImage = "url('Assets/clouds.jpeg')";
+                    }
+                }
+
+            })
+            .catch(error => {
+                // Handle errors here
+                console.error('Fetch error:', error);
+            });
+    }
 }
 
